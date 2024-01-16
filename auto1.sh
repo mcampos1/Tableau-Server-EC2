20024363043cbb350bb8f77893c1f1b734bc3024
Martin Campos <martin.campos8@gmail.com>
2024-01-16 16:57:38 -0500

    Update auto1.sh
    Signed-off-by: Martin Campos <martin.campos8@gmail.com>

--- auto1.sh
+++ auto1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 yum update -y
-yum install wget nano NetworkManager-tui net-tools bind-utils -y
-wget https://downloads.tableau.com/esdalt/2022.3.2/tableau-server-2022-3-2.x86_64.rpm
-sudo yum install tableau-server-2022-1-2.x86_64.rpm -y
-cd /opt/tableau/tableau_server/packages/scripts.20223.22.1213.1425/
+yum install wget nano firewalld -y
+wget https://downloads.tableau.com/esdalt/2023.1.7/tableau-server-2023-1-7.x86_64.rpm
+sudo yum install tableau-server-2023-1-7.x86_64.rpm -y
+cd /opt/tableau/tableau_server/packages/scripts.20233.23.1017.0948
 sudo ./initialize-tsm --accepteula --activation-service
 exit
 #will need to login to server again
